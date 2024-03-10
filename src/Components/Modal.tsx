da5117aff35dbdc618b226d2d3f2e3c171395953
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2024-03-10 13:06:32 -0300

    esclusãp da propriedade is open, e indetação

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -22,10 +22,30 @@ export interface ModalProps {
   divButton?: string;
 }
 
-export function Modal({ title, body, onCancel, onConfirm, textButton1, textButton2, className1, className2, iconName1, iconName2, iconSize1, iconSize2, iconColor1, iconColor2, iconClass1, iconClass2, divButton, isOpen}: ModalProps) {
-  const defaultClassName1 = "mr-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-  const defaultClassName2 = "mr-4 px-4 py-2 bg-indigo-800 text-white rounded hover:animate-pulse-slow"
-  const defaultClassDivButton = "flex justify-end"
+export function Modal({
+  title,
+  body,
+  onCancel,
+  onConfirm,
+  textButton1,
+  textButton2,
+  className1,
+  className2,
+  iconName1,
+  iconName2,
+  iconSize1,
+  iconSize2,
+  iconColor1,
+  iconColor2,
+  iconClass1,
+  iconClass2,
+  divButton,
+}: ModalProps) {
+  const defaultClassName1 =
+    'mr-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600';
+  const defaultClassName2 =
+    'mr-4 px-4 py-2 bg-indigo-800 text-white rounded hover:animate-pulse-slow';
+  const defaultClassDivButton = 'flex justify-end';
 
   return (
     <div className="fixed inset-0 flex items-center justify-center text-indigo-950 bg-gray-800 bg-opacity-50 z-50">
@@ -33,8 +53,26 @@ export function Modal({ title, body, onCancel, onConfirm, textButton1, textButto
         <h2 className="text-xl font-bold text-violet-600 mb-4">{title}</h2>
         <p className="mb-8">{body}</p>
         <div className={divButton || defaultClassDivButton}>
-          <Button type={'button'} text={textButton1 || ''} className={className1 || defaultClassName1} iconClassName={iconClass1} iconColor={iconColor1} iconName={iconName1} iconSize={iconSize1} onClick={onCancel}/>
-          <Button type={'button'} text={textButton2 || ''} className={className2 || defaultClassName2} iconClassName={iconClass2} iconColor={iconColor2} iconName={iconName2} iconSize={iconSize2} onClick={onConfirm}/>
+          <Button
+            type={'button'}
+            text={textButton1 || ''}
+            className={className1 || defaultClassName1}
+            iconClassName={iconClass1}
+            iconColor={iconColor1}
+            iconName={iconName1}
+            iconSize={iconSize1}
+            onClick={onCancel}
+          />
+          <Button
+            type={'button'}
+            text={textButton2 || ''}
+            className={className2 || defaultClassName2}
+            iconClassName={iconClass2}
+            iconColor={iconColor2}
+            iconName={iconName2}
+            iconSize={iconSize2}
+            onClick={onConfirm}
+          />
         </div>
       </div>
     </div>
